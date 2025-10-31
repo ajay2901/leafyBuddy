58c2590ac0cdfacd7b9b99a59e55cd3777908756
ajay2901 <ajayarora1717@gmail.com>
2025-10-31 16:28:57 +0530

    UI improved

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "2d991b354ec01fee"; }
+/******/ 		__webpack_require__.h = function() { return "986f832e53f77646"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
